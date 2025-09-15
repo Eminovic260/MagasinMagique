d77310854262236f8b14e6686c1a2cfd7fa05862
Emin Bahous <emin.bahous@le-campus-numerique.fr>
2025-09-15 11:38:51 +0200

    Premiers Tests

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -7,11 +7,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testItem() {
+        Item[] items = new Item[]{new Item("Objet", 10, 20)};
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertEquals("Objet", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(19, app.items[0].quality);
+    }
+
+
+    @Test
+    void testComté() {
+        Item[] items = new Item[]{new Item("Comté", 10, 20)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+
+        assertEquals("Comté", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(21, app.items[0].quality);
+    }
+
+    @Test
+    void testPassVIP() {
+        Item[] items = new Item[]{new Item("Pass VIP Concert", 1, 49)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+
+        assertEquals("Pass VIP Concert", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    void testKrypto() {
+        Item[] items = new Item[]{new Item("Kryptonite", 0, 80)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+
+        assertEquals("Kryptonite", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
     }
 
 }
+
